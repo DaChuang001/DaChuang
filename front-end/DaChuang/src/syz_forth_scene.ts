4295246d1bd7315fdc3ea69216922cce47ac1445
jjk9791 <m18790316665@163.com>
2021-04-08 14:53:21 +0800

    更改了颜色增加了姓名栏

--- front-end/DaChuang/src/syz_forth_scene.ts
+++ front-end/DaChuang/src/syz_forth_scene.ts
@@ -8,6 +8,7 @@ class syz_forth_scene extends eui.Component{
 
     public switch_dia_btn: eui.Button;
     public NPC_pic: eui.Image;
+    public NPC_name: eui.Label;
     public dialog: eui.Label;
     public point: number = 0; 
 
@@ -16,18 +17,22 @@ class syz_forth_scene extends eui.Component{
         switch(this.point){
             case 0:
                 this.NPC_pic.source = "angel_png"
+                this.NPC_name.text = "Eric："
                 this.dialog.text = "这就是顺序查找的过程了";
                 break;
             case 1:
                 this.NPC_pic.source = "me_png"
+                this.NPC_name.text = "#："
                 this.dialog.text = "这很简单嘛";
                 break;  
             case 2:
                 this.NPC_pic.source = "angel_png"
+                this.NPC_name.text = "Eric："
                 this.dialog.text = "是的，但是当数据量很大时，比较的次数会增加，这会花费很多的时间";
                 break;   
             case 3:
                 this.NPC_pic.source = "me_png"
+                this.NPC_name.text = "#："
                 this.dialog.text = "我们走吧";
                 break;   
             case 4:
